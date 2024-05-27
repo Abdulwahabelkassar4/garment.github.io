1c84b8a8f741d4fd946ae2a2317347d8b894452a
Abdulwahabelkassar4 <abdulwahabelkassar4@gmail.com>
2024-05-27 23:09:42 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql211.infinityfree.com"; // Your MySQL host name
-$username = "if0_36624665";              // Your MySQL username
-$password = "Ai2103003";      // Your MySQL password (same as your vPanel password)
-$dbname = "if0_36624665_garment_showcase"; // Your full database name
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "garment_showcase";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
